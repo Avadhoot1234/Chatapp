255e1fb238f8e36410b1c1a580517ff1a8d0d193
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2024-11-22 17:43:34 +0530

    Auth Error

--- src/componenets/Auth.js
+++ src/componenets/Auth.js
@@ -1,32 +1,25 @@
-import{auth,provider} from '../firebase.js';
-import {signInWithPopup} from 'firebase/auth'
+
+import { auth, provider } from "../firebase-config.js";
+import { signInWithPopup } from "firebase/auth";
+import "../styles/Auth.css";
 import Cookies from "universal-cookie";
 
-const cookies=new Cookies();
+const cookies = new Cookies();
 
-
-
-
-
-export const Auth=(props)=>{
-
-  const{setIsAuth}=props;
-    const signInWithGoogle=async()=>{
-
-      try{
-        const result=await signInWithPopup(auth,provider);
-        cookies.set("Auth-Token",result.user.refreshToken);
-        setIsAuth(true);
-      }catch(err){
-        console.error(err)
-      }
+export const Auth = ({ setIsAuth }) => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (err) {
+      console.error(err);
     }
+  };
   return (
-    <div>
-      <p>Signin With Google</p>
-      <button onClick={signInWithGoogle}>SignIn</button>
+    <div className="auth">
+      <p> Sign In With Google To Continue </p>
+      <button onClick={signInWithGoogle}> Sign In With Google </button>
     </div>
-  )
-}
-
-export default Auth
+  );
+};
